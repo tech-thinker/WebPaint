e9a3d024a1a3b52cb3ed453e65ede9d77a4d7f8b
Asif Mohammad Mollah <asifmohammadmollah@gmail.com>
2015-12-11 20:34:38 +0530

    Initial commit...

---
+++ script/main.js
@@ -0,0 +1,37 @@
+//	main.js
+var canvas=document.getElementById('mycanvas');
+var context=canvas.getContext('2d');
+
+var radius=10;
+var eDraw=false;
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight-5;
+
+context.lineWidth=radius*2;
+
+var mDown=function(e){
+	eDraw=true;
+	mMove(e);
+}
+
+var mUp=function(e){
+	eDraw=false;
+	context.beginPath();
+}
+
+var mMove=function(e){
+	if(eDraw==true){
+		context.lineTo(e.clientX,e.clientY);
+		context.stroke();
+		context.beginPath();
+		context.arc(e.offsetX,e.offsetY,radius,0,Math.PI*2);
+		context.fill();
+		context.beginPath();
+		context.moveTo(e.clientX,e.clientY);
+		//context.closePath();
+	}
+}
+
+canvas.addEventListener('mousedown',mDown);
+canvas.addEventListener('mouseup',mUp);
+canvas.addEventListener('mousemove',mMove);
---
+++ script/radius.js
@@ -0,0 +1,29 @@
+//	radius.js
+
+var setRadius=function(newRadius) {
+	// body...
+	if(newRadius<minRad)
+		newRadius=minRad;
+	else if(newRadius>maxRad)
+		newRadius=maxRad;
+	radius=newRadius;
+	context.lineWidth=radius*2;
+	radSpan.innerHTML=radius;
+}
+
+var minRad=0.5,
+	maxRad=100,
+	defaultRad=15,
+	interval=5,
+	radSpan=document.getElementById('radval'),
+	decRad=document.getElementById('decrad'),
+	incRad=document.getElementById('incrad');
+
+decRad.addEventListener('click',function(){
+	setRadius(radius-interval);
+});
+incRad.addEventListener('click',function(){
+	setRadius(radius+interval);
+});
+
+setRadius(defaultRad);
